c1aec2ac4a996d16eac17878cb43ee5c47a90e84
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-05 16:26:40 -0700

    Added the logic to make ConnectionFactory

--- src/main/java/com/revature/p0/util/ConnectionFactory.java
+++ src/main/java/com/revature/p0/util/ConnectionFactory.java
@@ -1,5 +1,10 @@
 package com.revature.p0.util;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
@@ -15,5 +20,36 @@ public class ConnectionFactory {
         }
     }
 
+    private ConnectionFactory(){
+        try{
+            props.load(new FileReader("src/main/resources/application.properties"));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static ConnectionFactory getInstance(){
+        if(connectionFactory == null){
+            connectionFactory = new ConnectionFactory();
+        }
+
+        return connectionFactory;
+    }
+
+    public Connection getConnection(){
+        Connection conn = null;
+
+        try{
+            conn = DriverManager.getConnection(
+                    props.getProperty("host-url"),
+                    props.getProperty("username"),
+                    props.getProperty("password")
+            );
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+
+        return conn;
+    }
 
 }
