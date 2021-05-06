e216a9104f26f59cad4275b22fc75aece84f6d83
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-05 17:08:16 -0700

    Created the list interface and added the empty methods to LinkedList

--- src/main/java/com/revature/p0/util/LinkedList.java
+++ src/main/java/com/revature/p0/util/LinkedList.java
@@ -1,4 +1,39 @@
 package com.revature.p0.util;
 
-public class LinkedList {
+public class LinkedList<T> implements List<T>, Queue<T> {
+    
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(T data) {
+        return false;
+    }
+
+    @Override
+    public void add(T data) {
+
+    }
+
+    @Override
+    public T remove(T data) {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T poll() {
+        return null;
+    }
+
+    @Override
+    public T peek() {
+        return null;
+    }
 }
---
+++ src/main/java/com/revature/p0/util/List.java
@@ -0,0 +1,5 @@
+package com.revature.p0.util;
+
+public interface List<T> extends Collection<T>{
+    T get(int index);
+}
