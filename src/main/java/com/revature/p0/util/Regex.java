215f33984f1d84d93cb2d2030dbe430db443f7b1
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-11 23:19:34 -0700

    Created regex patter for password and fixed some coverage for username to preven white space

--- src/main/java/com/revature/p0/util/Regex.java
+++ src/main/java/com/revature/p0/util/Regex.java
@@ -3,10 +3,17 @@ package com.revature.p0.util;
 public enum Regex {
 
     /*This pattern for username requires it to start with a letter, only allow letter, numbers, and Underscorse,
-    and at a minimum is 3 characters. Following this recomendation for pattern:
+    and at a minimum is 3 characters and no white spaces. Following this recomendation for pattern:
     https://javadeveloperzone.com/java-8/java-regular-expression-for-username/
     */
     USERNAME_PATTERN("^([a-zA-z])+([\\w@]{2,})+$"),
+
+    /* This is the pattern for password where it must have a Capital letter, a Number, and be at least
+    4 characters long but no longer than 50 characters, and no whiteSpaces, and limited special characters.
+    *
+    * */
+    PASSWORD_PATTERN("^(?=[a-zA-Z0-9#@$?_-]{4,50}$)(?=.*?[A-Z])(?=.*?[0-9]).*"),
+
     NONE ("");
 
     private String pattern;
