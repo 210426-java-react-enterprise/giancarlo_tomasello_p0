8bc0bc26469e5eec24ebecc220a12b3f6ae1ac06
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-12 07:16:42 -0700

    Wrote the patern for firstname

--- src/main/java/com/revature/p0/util/Regex.java
+++ src/main/java/com/revature/p0/util/Regex.java
@@ -2,17 +2,29 @@ package com.revature.p0.util;
 
 public enum Regex {
 
-    /*This pattern for username requires it to start with a letter, only allow letter, numbers, and Underscorse,
-    and at a minimum is 3 characters and no white spaces. Following this recomendation for pattern:
+    /*Rules:
+    - Must start with a letter
+    -Must be at least 2 characters
+    -No Special characters except _
+    -No whitespaces
+
+    Following this recomendation for pattern:
     https://javadeveloperzone.com/java-8/java-regular-expression-for-username/
     */
+
     USERNAME_PATTERN("^([a-zA-z])+([\\w@]{2,})+$"),
 
     /* This is the pattern for password where it must have a Capital letter, a Number, and be at least
     4 characters long but no longer than 50 characters, and no whiteSpaces, and limited special characters.
     *
     * */
+
     PASSWORD_PATTERN("^(?=[a-zA-Z0-9#@$?_-]{4,50}$)(?=.*?[A-Z])(?=.*?[0-9]).*"),
+
+    /* This it the pattern for firstname that can't start with a space, onlt allows letters, dashes, or underscores, and
+    must be between 2 and 26 characters.
+     */
+    FIRSTNAME_PATTERN("^[^-\\s][a-zA-Z_\\s-]{2,25}+$"),
 
     NONE ("");
 
