12e598cad3f93a49cc9e967efe49a6ef34bb4162
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-12 09:45:03 -0700

    Created more tests for the linkedList class. Removed TestDriver as it was unused

--- src/test/java/com/revature/p0/util/LinkedListTest.java
+++ src/test/java/com/revature/p0/util/LinkedListTest.java
@@ -67,4 +67,40 @@ public class LinkedListTest {
         Assert.assertNull(temp);
     }
 
+    @Test
+    public void test_PollNonEmpty(){
+        sut.add("test");
+        sut.add("test2");
+
+        String actual = sut.poll();
+
+        Assert.assertSame("test", actual);
+        Assert.assertEquals(1, sut.size());
+    }
+
+    @Test
+    public void test_PollEmpty(){
+
+        String actual = sut.poll();
+
+        Assert.assertNull(actual);
+    }
+
+    @Test
+    public void test_getNonEmpty(){
+        sut.add("test");
+        sut.add("test2");
+
+        String actual = sut.get(1);
+
+        Assert.assertSame("test2", actual);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void test_getEmpty(){
+        sut.get(10);
+    }
+
+
+
 }
