fffb63660d5a0243d417ad11a7372b736fc4d065
Giancarlo Tomasello <gtomasel@ucsc.edu>
2021-05-12 21:06:42 -0700

    Commented Connection Factory

--- src/main/java/com/revature/p0/util/ConnectionFactory.java
+++ src/main/java/com/revature/p0/util/ConnectionFactory.java
@@ -7,6 +7,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * A singleton class that is incharge of holding a connection to
+ * the database. Prevents the creation of multiple connection and
+ * will instead be injected into needed classes
+ */
 public class ConnectionFactory {
 
     private static ConnectionFactory connectionFactory;
@@ -20,6 +25,9 @@ public class ConnectionFactory {
         }
     }
 
+    /**
+     * Constructs a new ConnectionFactory using credentials specified in a .properties files
+     */
     private ConnectionFactory(){
         try{
             props.load(new FileReader("src/main/resources/application.properties"));
@@ -28,6 +36,11 @@ public class ConnectionFactory {
         }
     }
 
+    /**
+     * Returns an instance of the connectionFactory. If one does not exist create the singleton
+     * and return the instance
+     * @return connectionFactory An instance of the singleton class ConnectionFactory
+     */
     public static ConnectionFactory getInstance(){
         if(connectionFactory == null){
             connectionFactory = new ConnectionFactory();
@@ -36,6 +49,11 @@ public class ConnectionFactory {
         return connectionFactory;
     }
 
+    /**
+     * Logs in and returns a created connection using the specified host, user, and pass in the
+     * .properties file
+     * @return conn A Connection to the data base that can be used to call sql commands
+     */
     public Connection getConnection(){
         Connection conn = null;
 
